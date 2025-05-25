3aa8f82fc72588a83521d1c96382615ec6faaaa7
qingluoo <113030300+qingluoo@users.noreply.github.com>
2025-05-25 12:16:38 +0800

    baishi-update

--- baishi-master/src/main/java/com/luoqing/baishi/config/MyBatisPlusConfig.java
+++ baishi-master/src/main/java/com/luoqing/baishi/config/MyBatisPlusConfig.java
@@ -10,10 +10,9 @@ import org.springframework.context.annotation.Configuration;
 /**
  * MyBatis Plus 配置
  *
- * @author https://github.com/liyupi
  */
 @Configuration
-@MapperScan("com.yupi.springbootinit.mapper")
+@MapperScan("com.luoqing.baishi.mapper")
 public class MyBatisPlusConfig {
 
     /**
